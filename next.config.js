f0ec32681bc8fb07d66d423ff60923afe0d1c123
JoseLuria <jos02-log18@hotmail.com>
2023-11-26 01:37:27 -0600

    feat: :sparkles: added svg webpack loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,35 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              ext: 'tsx'
+            }
+          }
+        ]
+      }
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  }
+}
 
 module.exports = nextConfig
