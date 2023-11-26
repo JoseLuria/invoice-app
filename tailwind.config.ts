7f1599a934cef1cbe53c739742ab056673c07767
JoseLuria <jos02-log18@hotmail.com>
2023-11-26 00:06:24 -0600

    feat: :lipstick: added tailwind class dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,11 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      colors: {}
+    }
   },
   plugins: []
 }
