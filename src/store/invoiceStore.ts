e0645c6e8c5f4916a19a47f22da504380683d4fe
JoseLuria <jos02-log18@hotmail.com>
2023-11-26 22:53:25 -0600

    feat: :sparkles: added status array to invoice store

--- src/store/invoiceStore.ts
+++ src/store/invoiceStore.ts
@@ -1,26 +1,26 @@
-import type { Invoice } from '@/schemas'
+import type { Invoice, InvoiceStatus } from '@/schemas'
 import { create } from 'zustand'
 import { persist, devtools } from 'zustand/middleware'
 import { INITIAL_INVOICES } from '@/contants'
 
 type InvoiceStore = {
   invoices: Invoice[]
-  addInvoice: (invoice: Invoice) => void
-  removeInvoice: (invoice: Invoice) => void
+  status: InvoiceStatus[]
+  setStatus: (status: InvoiceStatus[]) => void
+  setInvoices: (invoices: Invoice[]) => void
 }
 
 export const invoiceStore = create<InvoiceStore>()(
   devtools(
     persist(
-      (set, store) => ({
+      (set) => ({
         invoices: [...INITIAL_INVOICES],
-        addInvoice: (invoice) => {
-          set((state) => ({
-            invoices: [...state.invoices, invoice]
-          }))
+        status: [],
+        setStatus: (status) => {
+          set(() => ({ status: [...status] }))
         },
-        removeInvoice: (invoice) => {
-          set((state) => ({ invoices: state.invoices.filter((i) => i.id !== invoice.id) }))
+        setInvoices: (invoices) => {
+          set(() => ({ invoices: [...invoices] }))
         }
       }),
       { name: 'invoiceStore' }
