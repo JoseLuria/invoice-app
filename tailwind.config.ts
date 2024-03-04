6b0afb1a48dc3807e6af4b996da1360f25aec319
JoseLuria <jos02-log18@hotmail.com>
2024-03-03 23:00:33 -0600

    feat: :lipstick: added open right animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
       colors: {
         dark: '#0C0E16',
         purple: '#7C5DFA',
+        red: '#EC5757',
         gray: '#888EB0',
         grayish: '#858BB2',
         'light-gray': '#DFE3FA',
@@ -22,15 +23,23 @@ const config: Config = {
       fadeIn: {
         '0%': { opacity: '0' },
         '100%': { opacity: '1' }
+      },
+      openRight: {
+        '0%': { 'clip-path': 'inset(0 100% 0 0)' },
+        '50%': { 'clip-path': 'inset(0 100% 0 0)' },
+        '100%': { 'clip-path': 'inset(0 0 0 0)' }
       }
     },
     animation: {
-      'fade-in': 'fadeIn 0.3s ease-in-out'
+      'fade-in': 'fadeIn 0.3s ease-in-out',
+      'open-right': 'openRight 0.6s ease-in-out'
     },
     boxShadow: {
-      card: '0px 10px 10px -10px rgba(72, 84, 159, 0.10)'
+      card: '0px 10px 10px -10px rgba(72, 84, 159, 0.10)',
+      'card-reverse': '0px -10px 10px -10px rgba(72, 84, 159, 0.10)'
     }
-  }
+  },
+  plugins: [require('tailwind-scrollbar-hide')]
 }
 
 export default config
