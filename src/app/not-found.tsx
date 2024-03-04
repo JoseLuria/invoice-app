02cbc32f81195bd568b5082e3f43d6906d986a8b
JoseLuria <jos02-log18@hotmail.com>
2024-03-03 23:24:16 -0600

    feat: :lipstick: added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import { Back, Empty } from '@/components'
+
+const InvoiceById = () => {
+  return (
+    <>
+      <Back />
+
+      <Empty>
+        The page you are looking for
+        <br />
+        does not exist or has been moved.
+      </Empty>
+    </>
+  )
+}
+
+export default InvoiceById
